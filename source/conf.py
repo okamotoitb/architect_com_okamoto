661b4f6aa10b17b44345a5d6924243dc44aaf633
okamotoitb <taku_peanuts1026@icloud.com>
2025-02-12 15:25:41 +0900

    Add files via upload

---
+++ source/conf.py
@@ -0,0 +1,30 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'okamoto'
+copyright = '2025, okamoto'
+author = 'okamoto'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+language = 'ja'
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+import sphinx_rtd_theme
+
+html_theme = 'sphinx_rtd_theme'
+#html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
---
+++ source/main.rst
@@ -0,0 +1,10 @@
+はじめてのSphinx
+===============================
+
+   Hello Sphinx!!!
+   Contents of your main document.
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
